4de19d8ef2de4fd806eac66f86b0f24ca9625462
raimartontsch <raimar.tontsch@gmail.com>
2022-06-04 22:37:01 +0200

    Add bugfix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Layout from '../src/components/Layout/Layout';
-import Home from '.';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
-			<Component {...pageProps} />{' '}
-			<Layout>
-				<Home />
-			</Layout>
-		</>
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
 	);
 }
 
